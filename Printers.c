64ba10a952ab11fdc2eb00ec16cff9143025b25c
Vlad Rusu <rusu.v2k@gmail.com>
2024-04-11 00:12:38 +0300

    Add Document methods

--- Printers.c
+++ Printers.c
@@ -1,1 +1,55 @@
 #include "Printers.h"
+
+// Document specific functions
+DocumentQueue *create_document_queue() {
+    DocumentQueue *doc_queue = (DocumentQueue *)malloc(sizeof(DocumentQueue));
+    doc_queue->head = NULL;
+    doc_queue->num_docs = 0;
+    doc_queue->num_docs_printed = 0;
+    doc_queue->num_docs_not_printed = 0;
+    return doc_queue;
+}
+
+Document *create_document(int doc_id, char *doc_title, int doc_num_lines) {
+    Document *doc = (Document *)malloc(sizeof(Document));
+    doc->doc_id = doc_id;
+    doc->doc_title = doc_title;
+    doc->doc_num_lines = doc_num_lines;
+    doc->doc_num_lines_to_print = doc_num_lines;
+    doc->next_doc = NULL;
+    return doc;
+}
+
+void add_document(DocumentQueue *doc_queue, Document *doc) {
+    // Check if the document queue is empty
+    if (doc_queue->head == NULL) {
+        doc_queue->head = doc;
+        doc->doc_id = 1; // Start the id from 1
+    } else {
+        // Find the last document in the queue
+        Document *current_doc = doc_queue->head;
+        while (current_doc->next_doc != NULL) {
+            current_doc = current_doc->next_doc;
+        }
+
+        // Check if the document id is unique and increment it from the last id
+        if (doc->doc_id <= current_doc->doc_id) {
+            doc->doc_id = current_doc->doc_id + 1;
+        }
+
+        // Add the new document to the end of the queue
+        current_doc->next_doc = doc;
+    }
+
+    // Increment the number of documents in the queue
+    doc_queue->num_docs++;
+    doc_queue->num_docs_not_printed++;
+}
+
+// Printer specific functions
+
+
+// Helper Printers functions
+
+
+// Helper Document functions
--- Printers.h
+++ Printers.h
@@ -6,6 +6,12 @@
 #include <math.h>
 #include <time.h>
 
+enum PrinterStatus {
+    PRINTER_STATUS_BUSY,
+    PRINTER_STATUS_FREE,
+    PRINTER_STATUS_OFFLINE
+};
+
 typedef struct Document {
     int doc_id;
     char *doc_title;
@@ -14,12 +20,38 @@ typedef struct Document {
     Document *next_doc;
 } Document;
 
+typedef struct DocumentQueue {
+    Document *head;
+    int num_docs;
+    int num_docs_printed;
+    int num_docs_not_printed;
+} DocumentQueue;
+
 typedef struct Printer {
     int printer_id;
     tm printer_line_print_time;
     Document *printer_current_doc;
     Printer *next_printer;
+    enum PrinterStatus printer_status;
+    
 } Printer;
 
+typedef struct PrinterList {
+    Printer *head;
+    int num_printers;
+    int num_printers_busy;
+    int num_printers_free;
+    int num_printers_offline;
+} PrinterList;
+
+// Document specific functions
+DocumentQueue *create_document_queue();
+Document *create_document(int doc_id, char *doc_title, int doc_num_lines);
+void add_document(DocumentQueue *doc_queue, Document *doc);
+
+// Printer specific functions
+PrinterList *create_printer_list();
+Printer *create_printer(int printer_id, tm printer_line_print_time);
+void add_printer(PrinterList *printer_list, Printer *printer);
 
 #endif // PRINTERS_H
