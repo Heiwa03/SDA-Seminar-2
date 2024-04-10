ad3c8b7599bf6f01008a65df5c30c45df5b3c96e
Vlad Rusu <rusu.v2k@gmail.com>
2024-04-10 23:37:21 +0300

    Modify compile pipeline to use Printers

---
+++ Printers.c
@@ -0,0 +1,1 @@
+#include "Printers.h"
---
+++ Printers.h
@@ -0,0 +1,10 @@
+#ifndef PRINTERS_H
+#define PRINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+
+#endif // PRINTERS_H
